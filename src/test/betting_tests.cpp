a3b63966b979f615f8a66e6c981711d07b80105a
IntegralTeam <repo@sfxdx.ru>
2019-10-08 02:56:32 +0700

    Add betting dbcheck unit test case

--- src/test/betting_tests.cpp
+++ src/test/betting_tests.cpp
@@ -6,6 +6,8 @@
 #include "random.h"
 #include "uint256.h"
 #include "test/test_wagerr.h"
+#include <cstdlib>
+#include <cstdio>
 
 #include <vector>
 #include <map>
@@ -118,6 +120,14 @@ BOOST_AUTO_TEST_CASE(betting_events_test)
     BOOST_CHECK(db.Read(eventsIndex));
     BOOST_CHECK(eventsIndex.size() == 1);
     BOOST_CHECK(eventsIndex.count(1) == 1);
+
+    for (auto i = 2; i < 1000; i++) {
+        plEvent.nEventId = i;
+        db.Save(plEvent);
+        BOOST_CHECK(db.Read(eventsIndex));
+        BOOST_CHECK(eventsIndex.size() == i);
+        BOOST_CHECK(eventsIndex.count(i) == 1);
+    }
 }
 
 BOOST_AUTO_TEST_CASE(betting_results_test)
@@ -135,5 +145,102 @@ BOOST_AUTO_TEST_CASE(betting_results_test)
     BOOST_CHECK(resultsIndex.size() == 1);
     BOOST_CHECK(resultsIndex.count(1) == 1);
 }
+
+BOOST_AUTO_TEST_CASE(betting_dbcheck_test)
+{
+    boost::filesystem::remove_all(CEventsDB::GetDbName());
+    std::srand(std::time(nullptr));
+    int nEventCount = 0;
+
+    for (auto i{0}; i < 2; i++) {
+        CEventsDB db{};
+        CPeerlessEvent plEvent{};
+        EventsIndex eventsIndex{};
+
+        if (i == 0) {
+            nEventCount = 1000 + std::rand() % 1000;
+            BOOST_CHECK(!db.Read(eventsIndex));
+            for (auto j{0}; j < nEventCount; j++) {
+                plEvent.nEventId = j;
+                db.Save(plEvent);
+            }
+        } else {
+            BOOST_CHECK(db.Read(eventsIndex));
+            for (auto j{0}; j < nEventCount; j++) {
+                BOOST_CHECK(eventsIndex.count(j) == 1);
+                BOOST_CHECK(eventsIndex[j].nEventId == j);
+            }
+        }
+    }
+}
+
+/*BOOST_AUTO_TEST_CASE(betting_crash_test)
+{
+    const std::string envFlag{"WAGGER_UNIT_TEST_BETTING_CRASH"};
+
+//    for (auto i{0}; i < 40; i++) {
+//        const char c{static_cast<char>(1.0 * ('z' - 'a') * std::rand() / RAND_MAX)};
+//        envFlag.append(1, c + 'a');
+//    }
+
+    if (std::getenv(envFlag.c_str()) == nullptr) {
+        std::string cmdline{"env "};
+//        const std::string flagFile{};
+        cmdline.append(envFlag);
+        cmdline.append("=1 ");
+//        cmdline.append(1, '=');
+//        cmdline.append(flagFile);
+//        cmdline.append(1, ' ');
+        cmdline.append(boost::unit_test::framework::master_test_suite().argv[0]);
+//        cmdline.append(1, ' ');
+        cmdline.append(" --run_test=betting_tests");
+//        boost::filesystem::ofstream(boost::filesystem::path(flagFile));
+        boost::filesystem::remove_all(CEventsDB::GetDbName());
+        runCommand(cmdline);
+
+        CEventsDB db{};
+        EventsIndex eventsIndex{};
+        CPeerlessEvent plEvent{};
+        BOOST_CHECK(db.Read(eventsIndex));
+        BOOST_CHECK(eventsIndex.size() > 0);
+        BOOST_CHECK(eventsIndex.size() < 1000);
+        for (const auto& pair : eventsIndex) {
+            BOOST_CHECK(pair.first < 1000);
+            BOOST_CHECK(pair.first == pair.second.nEventId);
+        }
+        BOOST_TEST_MESSAGE("Fork Complete");
+    } else {
+        BOOST_TEST_MESSAGE("Running Fork");
+        CEventsDB db{};
+        EventsIndex eventsIndex{};
+        CPeerlessEvent plEvent{};
+        BOOST_TEST_MESSAGE("Reading DB");
+        BOOST_CHECK(!db.Read(eventsIndex));
+        std::srand(std::time(nullptr));
+        BOOST_TEST_MESSAGE("Writing DB");
+        for (auto i = 0; i < 1000; i++) {
+            plEvent.nEventId = i;
+            db.Save(plEvent);
+            if (i > std::rand() % 333 + 666) {
+                int* p = nullptr;
+                *p = 42;
+            }
+        }
+    }
+
+//    std::thread thread{[]() {
+//            CEventsDB db{};
+//            EventsIndex eventsIndex{};
+//            CPeerlessEvent plEvent{};
+
+//            BOOST_CHECK(!db.Read(eventsIndex));
+
+//            for (auto i = 1; i < 100; i++) {
+//                plEvent.nEventId = i;
+//                db.Save(plEvent);
+//            }
+//    }};
+//    thread.join();
+}*/
 
 BOOST_AUTO_TEST_SUITE_END()
